8195edefa15739a7038488e0a4f57f0bfb5a9507
Yerkebulan <erke.zhumagali@gmail.com>
2017-01-29 01:08:46 +0600

    week 1 done

---
+++ week 1/StudentAYS/StudentAYS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAYS
+{
+        class Student
+        {
+            private string firstName, lastName;
+            private int gpa;
+            public Student() { }
+            public Student(string firstName, string lastName, int gpa)
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.gpa = gpa;
+            }
+
+            public override string ToString()
+            {
+                return "First name = " + firstName + "\n" + "Last name = " + lastName + "\n" + "GPA = " + gpa;
+            }
+        }
+
+        class Sample
+        {
+            static void Main()
+            {
+                Student student = new Student("Aysultan", "Qali", 4);
+
+                Console.WriteLine(student);
+                Console.ReadKey();
+            }
+        }
+    }
