d4b84a4f796a898210f3d139713528a5b108b6da
Yerkebulan <erke.zhumagali@gmail.com>
2017-02-02 00:27:27 +0600

    pt

---
+++ week 1/PrimeNumberNew/PrimeNumberNew/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberNew
+{
+    class Program
+    {
+           
+            static int IsPrime(int x)
+            {
+                int a = x;
+                int с = 0;
+                for (int i = 1; i <= a; i++)
+                {
+                    if (a % i == 0)
+                        с++;
+                }
+
+                if (с == 2 || a == 1)
+                    return a;
+
+                else
+                    return 0;
+            }
+
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Enter your numbers: ");
+
+
+                string s = Console.ReadLine();
+
+                string[] arr = s.Split();
+
+                int y;
+                int z;
+                Console.WriteLine("Prime numbers are:");
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    y = int.Parse(arr[i]);
+                    z = IsPrime(y);
+                    if (z == y)
+                    {
+                        Console.Write(y + " ");
+                    }
+
+                }
+
+               
+                Console.ReadKey();
+            }
+        }
+    }
+
