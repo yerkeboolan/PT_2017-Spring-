c7dda8b0e74c0616a9f86efa0f8d52c73a1646ee
Yerkebulan <erke.zhumagali@gmail.com>
2017-02-02 11:14:25 +0600

    already done

--- week 2/MaxMin/MaxMin/Program.cs
+++ week 2/MaxMin/MaxMin/Program.cs
@@ -18,34 +18,68 @@ namespace MaxMin
             string s = sr.ReadLine(); // "1 2 3 4 5 6 7"
             string[] arr = s.Split(); // ["1", "2", "3", "4", "5", "6", "7"];
 
-            int max1 = 0;
+            int maxi = 0;
 
             for (int i = 0; i < arr.Length; i++)
             {
                 int t = int.Parse(arr[i]);
-                if (t > max1)
+                if (t > maxi)
                 {
-                    max1 = t;
+                    maxi = t;
                 }
 
-                foreach (string ss in arr)
-                {
-                    max1 = int.Parse(ss);
-                }
-                Console.Write(max1);
+                /*   foreach (string ss in arr)
+                   {
+                       maxi = int.Parse(ss);
+                   } */
+
+            }
+                Console.Write(maxi);
 
                 sr.Close();
                 fs.Close();
 
                 Console.ReadKey();
-            }
+            
         }
-    
 
+        static void f2()
+        {
+            FileStream fs = new FileStream(@"c:\test\file new.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+
+            string s = sr.ReadLine(); // "1 2 3 4 5 6 7"
+            string[] arr = s.Split(); // ["1", "2", "3", "4", "5", "6", "7"];
+
+            int mini = 900000000;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int t = int.Parse(arr[i]);
+                if (t < mini)
+                {
+                    mini = t;
+                }
+
+                /*   foreach (string ss in arr)
+                   {
+                       maxi = int.Parse(ss);
+                   } */
+
+            }
+            Console.Write(mini);
+
+            sr.Close();
+            fs.Close();
+
+            Console.ReadKey();
+
+        }
 
         static void Main(string[] args)
         {
-            f1();
+            f2();
         }
     }
 }
