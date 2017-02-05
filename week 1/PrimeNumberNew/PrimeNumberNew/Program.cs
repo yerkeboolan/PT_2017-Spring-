2d481633697a4d90929307465536cfae96b3a826
Yerkebulan <erke.zhumagali@gmail.com>
2017-02-05 13:12:28 +0600

    remake

--- week 1/PrimeNumberNew/PrimeNumberNew/Program.cs
+++ week 1/PrimeNumberNew/PrimeNumberNew/Program.cs
@@ -6,57 +6,58 @@ using System.Threading.Tasks;
 
 namespace PrimeNumberNew
 {
-    class Program
-    {
+     class Program
+     {
+        
+         static int IsPrime(int x)
+         {
+             int a = x;
+             int c = 0;
+             for (int i = 1; i <= a; i++)
+             {
+                 if (a % i == 0)
+                     c++;
+             }
 
-        static int IsPrime(int x)
-        {
-            int a = x;
-            int c = 0;
-            for (int i = 1; i < a; i++)
-            {
-                if (a % i == 0)
-                    c++;
-            }
+             if (c == 2 || a == 1)
+             {
+                 return a;
+             } else
+             {
+                 return 0;
+             }
 
-            if (c == 2 || a == 1)
-            {
-                return a;
-            } else
-            {
-                return 0;
-            }
+             }
 
-            }
+             static void Main(string[] args)
+             {
 
-            static void Main(string[] args)
-            {
-
-                Console.WriteLine("Enter your numbers: ");
+                 Console.WriteLine("Enter your numbers: ");
 
 
-                string s = Console.ReadLine();
+                 string s = Console.ReadLine();
 
-                string[] arr = s.Split();
+                 string[] arr = s.Split();
 
-                int y;
-                int z;
-                Console.WriteLine("Prime numbers are:");
+                 int y;
+                 int z;
+                 Console.WriteLine("Prime numbers are:");
 
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    y = int.Parse(arr[i]);
-                    z = IsPrime(y);
-                    if (z == y)
-                    {
-                        Console.Write(y + " ");
-                    }
+                 for (int i = 0; i < arr.Length; i++)
+                 {
+                     y = int.Parse(arr[i]);
+                     z = IsPrime(y);
+                     if (z == y)
+                     {
+                         Console.Write(y + " ");
+                     }
 
-                }
+                 }
 
-               
-                Console.ReadKey();
-            }
-        }
-    }
 
+                 Console.ReadKey();
+             }
+         }
+     }
+     
+   
