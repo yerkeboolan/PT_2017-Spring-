ee5148f19a68dec67046bb0972005c768b0e1e30
Yerkebulan <erke.zhumagali@gmail.com>
2017-04-06 01:00:49 +0600

    example of week 12

---
+++ week 12 example/PaintClass/PaintClass/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintClass
+{
+    public partial class Form1 : Form
+    {
+        private PaintBase paint;
+        public Form1()
+        {
+            InitializeComponent();
+            paint = new PaintBase(pictureBox1);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint.prev = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                paint.Draw(e.Location);
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint.SaveLastPath();
+        }   
+    }
+}
